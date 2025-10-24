8ee966ab881c63d860baa04d08bfee803903814f
Jashwanth Kumar Sathish Kumar <Jashwanthkumar0912@gmail.com>
2025-10-24 07:32:23 +0100

    Add files via upload

---
+++ BlankSpace.py
@@ -0,0 +1,10 @@
+# Print out this messages and make sure there is a blank line between them.
+
+#Welcome to the program.
+
+#Enjoy your day.
+
+
+print("Welcome to the program.")
+print()
+print("Enjoy your day.")
